a110a0cbff2c18be40ab0c6fd30ae0e3748144a9
Igor Galeta <galeta.igor@gmail.com>
2010-06-23 19:23:24 +0300

    Fix initializer issue

--- lib/auto_html/railtie.rb
+++ lib/auto_html/railtie.rb
@@ -5,22 +5,16 @@ module AutoHtml
   if defined? Rails::Railtie
     require 'rails'
     class Railtie < Rails::Railtie
-      initializer "auto_html.initialization" do
-        ActiveSupport.on_load :active_record do
-          AutoHtml::Railtie.insert
-        end
-      end
-    end
-  end
-
-  class Railtie
-    def self.insert
       # Register built-in filters
       Dir["#{File.dirname(__FILE__) + '/auto_html/filters'}/**/*"].each do |filter|
         require "#{filter}"
       end
-
-      ActiveRecord::Base.send :include, AutoHtml::AutoHtmlFor
+      
+      config.after_initialize do
+        ActiveSupport.on_load :active_record do
+          ActiveRecord::Base.send :include, AutoHtml::AutoHtmlFor
+        end
+      end
     end
   end
 end
